4d0a90860e2601a64674708a3b1639625512b254
robelmd <robeltedla.me@gmail.com>
2022-03-28 03:02:41 -0700

    lst

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -18,4 +18,26 @@ int main(void)
 
 	    return (0);
 
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	printf("Holberton School");
+
+	return (0);
+
 }
